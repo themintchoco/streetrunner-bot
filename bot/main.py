df23e56d02ca921399a842954e6e65da53092b6b
mint <themintchoco@gmail.com>
2021-05-28 15:52:43 +0800

    [Feature] Prod/Dev bot segregation

--- bot/main.py
+++ bot/main.py
@@ -7,8 +7,8 @@ from pretty_help import PrettyHelp
 import sentry_sdk
 
 sentry_sdk.init(
-    'https://7b74da9447304a35b6f8c49da4fd09f1@o737869.ingest.sentry.io/5785215',
-    traces_sample_rate=1.0
+	'https://7b74da9447304a35b6f8c49da4fd09f1@o737869.ingest.sentry.io/5785215',
+	traces_sample_rate=1.0
 )
 
 intents = discord.Intents.default()
@@ -19,9 +19,20 @@ bot = commands.Bot(command_prefix='!', intents=intents, help_command=PrettyHelp(
 bot.add_cog(Player(bot))
 bot.add_cog(WebServer(bot))
 
+
+@bot.event
+async def on_message(message):
+	if message.guild and (message.guild.id == 846060357901615115) ^ (os.environ.get('DEV', False) == 'DEV'):
+		# Prevent production bot from replying to dev server and vice versa
+		return
+
+	await bot.process_commands(message)
+
+
 @bot.event
 async def on_error(event, *args, **kwargs):
 	# allow Sentry to capture the error
 	raise
+
 
 bot.run(os.environ['TOKEN'])
