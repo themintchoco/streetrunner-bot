cdb0939b506b0212a4201df495ef47bdd41b4f33
mint <themintchoco@gmail.com>
2021-06-07 01:30:41 +0800

    [Improvements] Adjusted XP processing priority

--- bot/main.py
+++ bot/main.py
@@ -21,18 +21,27 @@ bot.add_cog(Admin(bot))
 bot.add_cog(WebServer(bot))
 
 
+async def process_xp(message):
+    if not message.author.bot:
+        level_before, level_after = await XP.process_message(message)
+        if level_after > level_before:
+            render = await card.render_xp_levelup(message.author, level_before, level_after)
+            await message.channel.send(file=discord.File(render.file_animated(format='GIF'), 'xp_levelup.gif'))
+
+
 @bot.event
 async def on_message(message):
     if message.guild and (message.guild.id == 846060357901615115) ^ (os.environ.get('DEV', False) == 'DEV'):
         # prevent production bot from replying to dev server and vice versa
         return
 
-    await bot.process_commands(message)
-    if not message.author.bot:
-        level_before, level_after = await XP.process_message(message)
-        if level_after > level_before:
-            render = await card.render_xp_levelup(message.author, level_before, level_after)
-            await message.channel.send(file=discord.File(render.file_animated(format='GIF'), 'xp_levelup.gif'))
+    if message.content.startswith(f'{bot.command_prefix}xp'):
+        # exception for xp-related commands -- process xp before the command
+        await process_xp(message)
+        await bot.process_commands(message)
+    else:
+        await bot.process_commands(message)
+        await process_xp(message)
 
 
 @bot.event
