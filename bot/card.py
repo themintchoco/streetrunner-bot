53155fb5f9e13aa842961f21d6c7cde6a49b1374
Low Xuguang <personal@keyute.dev>
2021-06-07 12:32:34 +0800

    revert fix

--- bot/card.py
+++ bot/card.py
@@ -92,7 +92,7 @@ class PlayerStatsPrison(PlayerStats):
     def __init__(self, **kwargs):
         super().__init__(type=PlayerStatsType.Prison)
         self.rank = kwargs['rank']
-        self.blocks = kwargs['amount']
+        self.blocks = kwargs['blocks']
 
 
 class PlayerStatsArena(PlayerStats):
