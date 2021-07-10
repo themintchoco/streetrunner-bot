2d2d44c4f011d9d93aa06d9a507fd9c471c49113
Low Xuguang <personal@keyute.dev>
2021-07-10 08:31:24 +0800

    removed floating point repr

--- bot/card.py
+++ bot/card.py
@@ -276,14 +276,16 @@ def get_number_representation(number: int) -> str:
     magnitude = (len(str(number)) - 1) // 3
     return f'{(number / (10 ** (magnitude * 3))):.3g}{" KMGTPEZY"[magnitude] if magnitude > 0 else ""}'
 
+
 def get_timedelta_representation(td: datetime.timedelta) -> str:
     hours, remainder = divmod(td.total_seconds(), 3600)
     minutes, seconds = divmod(remainder, 60)
 
-    repr = ((f'{hours}h ' if hours else '') +
-            (f'{minutes}m ' if minutes else ''))
+    repr = ((f'{int(hours)}h ' if hours else '') +
+            (f'{int(minutes)}m ' if minutes else ''))
 
     return repr if repr else 'Not played yet'
+
 
 def get_level_from_xp(xp: int) -> int:
     i = 1
