4460d6b9c22e41288cc73bd07f8d083a8adf7a7a
Low Xuguang <keyute@users.noreply.github.com>
2021-08-18 13:04:51 +0800

    Alternative deployment (#10)

--- bot/cogs/Admin.py
+++ bot/cogs/Admin.py
@@ -2,6 +2,8 @@ import os
 
 from discord.ext import commands
 
+from bot.config import env
+
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -14,8 +16,8 @@ class Admin(commands.Cog):
     async def version(self, ctx):
         if 'HEROKU_APP_NAME' in os.environ:
             await ctx.send(f'{os.environ["HEROKU_APP_NAME"]} {os.environ["HEROKU_RELEASE_VERSION"]}')
-        elif 'AWS_DEPLOY_VER' in os.environ:
-            await ctx.send(f'AWS {os.environ["AWS_DEPLOY_VER"]}')
+        elif v := env.get('VERSION', None):
+            await ctx.send(f'AWS {v}')
         else:
             await ctx.send('Local')
 
--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,15 @@
+import json
+import os
+
 import discord
 from discord.ext import commands
 from pretty_help import PrettyHelp
+
+env = {}
+
+if os.path.isfile('env.json'):
+    with open('env.json') as f:
+        env = json.loads(f.read())
 
 intents = discord.Intents.default()
 intents.members = True
